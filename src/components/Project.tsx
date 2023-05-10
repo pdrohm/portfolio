7ebce540722e595c1dd1a0bfb29f39cc6c3bec0c
Pedro H. Marques <opedrohenriquemarques@gmail.com>
2023-05-10 12:27:08 -0300

    Fix projects responsive design

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -8,17 +8,19 @@ type Props = {
 
 export default function Project({ index, project }: Props) {
   return (
-    <div className="w-screen flex-shrink-0 flex flex-col space-y-5 items-center justify-center h-screen p-15 md:p-40">
-      <img className="w-1/2 h-1/2" src={project.mockupArt} alt="" />
+    <div className="w-screen flex-shrink-0 flex flex-col space-y-5 items-center justify-start h-scren p-15 md:p-40 ">
+      <img className="xl:w-1/2 md:w-1/2 w-auto h-auto" src={project.mockupArt} alt="" />
 
       <div className="space-y-10 px-0 md:px-10 max-w-6xl">
-        <h3 className="xl:text-3xl text-xl text-center">
-          <span className="underline decoration-blue xl:text-4xl text-2xl">
+       
+        <h3 className="xl:text-3xl text-xl text-center flex xl:flex-row md:flex-row flex-col items-center justify-center">
+          <span className="underline decoration-blue xl:text-4xl text-2xl xl:mr-5 md:mr-3">
             Case of Study {index + 1} of 3:
           </span>{" "}
           {project.title}
         </h3>
-        <p className="text-center md:text-left lg:text-lg text-sm leading-tight overflow-y-auto max-h-80 px-9  ">
+        
+        <p className="text-center md:text-left lg:text-lg text-sm leading-tight overflow-y-auto max-h-80 px-9">
           {project.description}
         </p>
       </div>
